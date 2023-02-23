00b73136b4ed71c1ffffeab774700ddace0277e7
MrBlackout1 <mrbobfish0@gmail.com>
2023-02-23 17:22:27 +0200

    first commit

---
+++ Laba 2.4 2023/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо список рядків
+        string[] strings = { "Volodymyr", "Georgiy", "Ivan", "Max" };
+
+        // Створюємо делегат, який приймає рядок і повертає його довжину
+        Func<string, int> stringLength = s => s.Length;
+
+        // Виводимо довжини всіх рядків у списку
+        foreach (string str in strings)
+        {
+            Console.WriteLine("{0} - {1} characters", str, stringLength(str));
+        }
+    }
+}
